fa51c01be493dd447d16250ea76d72e6faf99744
Nitish Veni <nitish.veni@ascendion.com>
2024-10-18 13:10:05 +0530

    Pushed

--- labs-html/demo2forms.js
+++ labs-html/demo2forms.js
@@ -1,16 +1,16 @@
 function clicck(event) {
     event.preventDefault();
     
-    // Get form values
     const fname = document.getElementById('fname').value;
     const lname = document.getElementById('lname').value;
+    const date = document.getElementById('date').value;
     const address = document.getElementById('address').value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
     
     // Collect all checked options (Latte, Cappuccino)
     const selectedOptions = [];
-    if (document.getElementById('latte').checked) {
-        selectedOptions.push('Latte');
+    if (document.getElementById('kapi').checked) {
+        selectedOptions.push('Filter Coffee');
     }
     if (document.getElementById('cappucino').checked) {
         selectedOptions.push('Cappuccino');
@@ -26,67 +26,39 @@ function clicck(event) {
 
     const fnamecell = rowdata.insertCell(0);
     const lnamecell = rowdata.insertCell(1);
-    const addresscell = rowdata.insertCell(2);
-    const gendercell = rowdata.insertCell(3);
-    const optionscell = rowdata.insertCell(4);
-    const deliverycell = rowdata.insertCell(5);
+    const datecell = rowdata.insertCell(2);
+    const addresscell = rowdata.insertCell(3);
+    const gendercell = rowdata.insertCell(4);
+    const optionscell = rowdata.insertCell(5);
+    const deliverycell = rowdata.insertCell(6);
 
     // Populate cells
     fnamecell.textContent = fname;
     lnamecell.textContent = lname;
+    datecell.textContent = date;
     addresscell.textContent = address;
     gendercell.textContent = gender;
     optionscell.textContent = options;
     deliverycell.textContent = delivery;
 
+    //Locally 
+    const formlocal = {
+        fname: fname,
+        lname: lname,
+        date: date,
+        address: address,
+        gender: gender,
+        options: options,
+        delivery: delivery
+    };
+    let savedata = JSON.parse(localStorage.getItem('formlocal')) || [];
+    savedata.push(formlocal);
+    localStorage.setItem('formlocal', JSON.stringify(savedata));
+
     // Reset form after submission
     document.querySelector('form').reset();
 }
-
-
-
-// function clicck(event){
-//     event.preventDefault();
-//     // let displayy = document.getElementById('name');
-//     // console.log(displayy.value);
-//     const fname = document.getElementById('fname').value;
-//     const lname = document.getElementById('lname').value;
-    
-//     const address = document.getElementById('address').value;
-//     const gender = document.querySelector('input[name="gender"]:checked').value;
-//     // const options = document.querySelector('input[name="options"]:checked').value;
-    
-//     // const latte = document.getElementById('latte').checked;
-//     // const cappucinno = document.getElementById('cappucino').checked;
-//     const delivery = document.getElementById('delivery').value;
-
-//     const table = document.getElementById('datatable');
-
-//     const rowdata = table.insertRow();
-
-//     const fnamecell = rowdata.insertCell(0);
-//     const lnamecell = rowdata.insertCell(1);
-//     const addresscell = rowdata.insertCell(2);
-//     const gendercell = rowdata.insertCell(3);
-//     // const optionscell = rowdata.insertCell(4);
-//     // const lattecell = rowdata.insertCell(4);
-//     // const cappucinnocell  = rowdata.insertCell(4);
-//     const deliverycell = rowdata.insertCell(5);
-    
-
-
-//     fnamecell.textContent = fname;
-//     lnamecell.textContent = lname;
-//     addresscell.textContent = address;
-//     gendercell.textContent = gender;
-//     // optionscell.textContent = options;
-
-//     // lattecell.textContent = latte;
-//     // cappucinnocell.textContent = cappucinno;
-//     deliverycell.textContent = delivery;
-
-
-//     document.getElementById('reset').reset();
-
-// } 
-// clicck();
+window.onload = function(){
+    let savedata = JSON.parse(localStorage.getItem('formlocal')) || [];
+    // savedata.forEach(data=>)
+};
